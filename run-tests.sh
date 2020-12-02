bee2227fec9e932df93ca02b7cd8cbb153e7afd3
Parth Shandilya <parth.shandilya@cern.ch>
2020-12-02 09:41:35 +0100

    global: migrate CI to GH Actions
    closes #93

--- run-tests.sh
+++ run-tests.sh
@@ -7,8 +7,18 @@
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-pydocstyle invenio_records_ui tests docs && \
-isort invenio_records_ui tests --check-only --diff && \
-check-manifest --ignore ".travis-*" && \
-sphinx-build -qnNW docs docs/_build/html && \
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+pydocstyle invenio_records_ui tests docs
+isort invenio_records_ui tests --check-only --diff
+check-manifest --ignore ".*-requirements.txt"
+sphinx-build -qnNW docs docs/_build/html
+docker-services-cli up ${DB}
 python setup.py test
+tests_exit_code=$?
+docker-services-cli down
+exit "$tests_exit_code"
