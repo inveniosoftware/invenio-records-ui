8e32e9df961b42cffda3a0ad2272dba2db96106f
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-12-01 21:09:05 +0100

    release: v1.2.2

--- invenio_records_ui/__init__.py
+++ invenio_records_ui/__init__.py
@@ -2,6 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2018 CERN.
+# Copyright (C) 2024 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -296,6 +297,6 @@ from __future__ import absolute_import, print_function
 
 from .ext import InvenioRecordsUI
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 __all__ = ("__version__", "InvenioRecordsUI")
